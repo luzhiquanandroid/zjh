3105f718697f2447bebfebbbf4980bce8596a223
luzhiquan <13384077876@163.com>
2018-09-12 11:42:36 +0800

    dev

--- app/src/main/java/com/sxjs/jd/MyApplication.java
+++ app/src/main/java/com/sxjs/jd/MyApplication.java
@@ -22,6 +22,7 @@ public class MyApplication extends Application {
         }
         if(CommonConfig.DEBUG){
             LeakCanary.install(this);
+            //路由框架
             ARouter.openLog();     // 打印日志
             ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         }
